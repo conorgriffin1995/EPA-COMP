6f8fa0b7b029450043c34eec817f937e82f40639
epa <epa@localhost.localdomain>
2017-10-06 10:55:40 +0100

    changes, adding week 4

--- week3/script2.sh
+++ week3/script2.sh
@@ -3,9 +3,9 @@
 #this is a CPU counting script
 echo "hello user"
 
-cnt=`grep processor /proc/cpuinfo | wc-l`
+cnt=`grep "processor" /proc/cpuinfo | wc -l`
 
-if [[ $cnt -le 2 ]]; then
+if [ $cnt -le 2 ]; then
   # some action
   echo "Too few CPUs, exiting"
 fi
---
+++ week4/lab4.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cnt=`grep processor /proc/cpuinfo | wc -l`
+
+if [ $cnt -le $1 ]; then
+
+  echo "Not enough CPUS, exiting"
+
+fi
+
+
+
+
