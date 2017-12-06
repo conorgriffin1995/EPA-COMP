3704aa80167b870240cffb51e96eba34239f6b54
conorgriffin1995 <conorgriffin1995@gmail.com>
2017-12-06 14:51:45 +0000

    runtest.sh complete and results.dat

--- CA2/runtest.sh
+++ CA2/runtest.sh
@@ -1,13 +1,31 @@
 #!/bin/bash
+echo -e "C0 \t N \t idle" >> results.dat
 
-#invoke loadtest
-./loadtest $1&
-echo "running load test.."
+for i in {1..50}
+do
 
-#sleep
-sleep $2
-echo "loadtest is sleeping.."
+# invoke the loadtest
+echo "loadtest $i is running.."
+./loadtest $i&
 
-echo "killing the loadtest.."
-#kill the loadtest
+# sleep in background to allow further cmds to execute
+echo "loadtest $i is sleeping.."
+sleep 5
+
+# while load test is running in background collect cpu utilization
+idle=`mpstat 5 1 -o JSON | jq '.sysstat.hosts[0].statistics[0]."cpu-load"[0].idle'`
+echo "$idle"
+
+# kill the loadtest
+echo "loadtest $i is being killed.."
 pkill loadtest
+
+# number of completions in synthetic.dat
+C=`cat synthetic.dat | wc -l`
+
+echo -e "$C \t $i \t $idle" >> results.dat
+
+# remove synthetic.dat file each time in the loop for new number of completions
+rm synthetic.dat
+
+done
